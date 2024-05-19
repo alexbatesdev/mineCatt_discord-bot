6b6ff03c3a5714ddf7f9c9f66f839e3c699b4e52
Alex Bates <mbates@student.neumont.edu>
2024-05-19 12:59:56 -0600

    start

--- commands/minecraft/start.js
+++ commands/minecraft/start.js
@@ -6,13 +6,15 @@ module.exports = {
         .setDescription("What's updog? The server, that is!"),
     async execute(interaction) {
         // Defer the reply to allow time for server startup and IP retrieval
-        await interaction.deferReply('Starting the server... (This takes about 60 seconds)');
+        await interaction.reply('Starting the server... (This takes about 60 seconds)');
         
         try {
             // Make the API call to start the server and get the IP address
             const response = await fetch('https://5bz6qzi222gwpvjypdhutf6j4e0yyrly.lambda-url.us-west-2.on.aws/')
                 .then(response => response.json());
             console.log(response);
+
+            
             
             // Construct the reply with the server IP address
             const reply = `The Minecraft server is up!\nYou can find it at ${response.public_ip}!`;
